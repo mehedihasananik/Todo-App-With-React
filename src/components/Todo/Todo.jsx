647cbc6fc3b7bd1ac735fca7aace4f868d408031
mehedihasananik <marshallanique@gmail.com>
2024-02-08 20:46:54 +0600

    to do list added

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,5 +1,18 @@
-const Todo = () => {
-  return <div>Todo</div>;
+const Todo = ({ todo, deleTodo, toggleTodo, editTodo }) => {
+  return (
+    <div className="Todo">
+      <p
+        onClick={() => toggleTodo(todo.id)}
+        className={`${todo.completed ? "completed" : "incompleted"}`}
+      >
+        {todo.task}
+      </p>
+      <div>
+        <button onClick={() => editTodo(todo.id)}>Edit</button>
+        <button onClick={() => deleTodo(todo.id)}>Del</button>
+      </div>
+    </div>
+  );
 };
 
 export default Todo;
--- src/components/TodoManager/TodoManager.jsx
+++ src/components/TodoManager/TodoManager.jsx
@@ -1,9 +1,49 @@
+import { useState } from "react";
 import TodoForm from "../TodoForm/TodoForm";
+import { v4 as uuidv4 } from "uuid";
 
 const TodoManager = () => {
+  const [todos, setTodoes] = useState([]);
+
+  const addTodo = (todo) => {
+    setTodoes([
+      ...todos,
+      { id: uuidv4(), task: todo, completed: false, isEditing: false },
+    ]);
+  };
+  console.log(todos);
+
+  const deleTodo = (id) => {
+    setTodoes(todos.filter((todo) => todo.id !== id));
+  };
+
+  const toggleTodo = (id) => {
+    setTodoes(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const editTodo = (id) => {
+    setTodoes(
+      todos.map((todo) =>
+        todo.id == id ? { ...todo, isEditing: !todo.isEditing } : todo
+      )
+    );
+  };
+
+  const editTask = (task, id) => {
+    setTodoes(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, task, isEditing: !todo.isEditing } : todo
+      )
+    );
+  };
+
   return (
     <div>
-      <TodoForm />
+      <TodoForm addTodo={addTodo} />
     </div>
   );
 };
